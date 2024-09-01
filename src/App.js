23a1e922359599371f680478330e18332a928bba
kalilmedfai <kalilmedfai@hotmail.fr>
2024-09-01 19:26:39 +0200

    Multi Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 // import logo from './logo.svg';
 import '@/App.css';
 
-import { BrowserRouter } from 'react-router-dom'
+import { Routes, BrowserRouter, Route } from 'react-router-dom'
 import PublicRouter from './pages/Public/PublicRouter';
+import AdminRouter from './pages/Admin/AdminRouter';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <PublicRouter/>
+
+        <Routes>
+          {/* '*' permet de dire qu'il y a de l'enfant derrière sinon erreur */}
+          <Route path='/*' element={<PublicRouter/>}/>
+          <Route path='/admin/*' element={<AdminRouter/>}/>
+          
+        </Routes>
+
       </BrowserRouter>
     </div>
   );
