527ad31fd597046a51ae146cf475061514389c72
kalilmedfai <kalilmedfai@hotmail.fr>
2024-10-29 16:39:43 +0100

    modification serveur (passage de mysql à sequelize)

--- back/config/db.js
+++ back/config/db.js
@@ -1,23 +1,41 @@
 // Import des modules necessaires
-const mysql = require('mysql');
-require('dotenv').config();
+const { Sequelize } = require('sequelize')
+
+// require('dotenv').config();
 
 // Connexion à la base de donnée
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    // l'ajout du port n'est pas obligatoire
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-});
-
-db.connect(err => {
-    if (err) {
-        console.error('Erreur de connexion à la base de données :', err);
-        throw err;
+let sequelize = new Sequelize(
+    process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+        host: process.env.DB_HOST,
+        // l'ajout du port n'est pas obligatoire
+        port: process.env.DB_PORT,
+        dialect: 'mysql',
+        logging: false
     }
-    console.log('Connecté à MySQL');
-});
+)
 
-module.exports = db;
+
+// Synchronisation des modèles
+// sequelize.sync()
+
+module.exports = sequelize
+
+
+// const db = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     // l'ajout du port n'est pas obligatoire
+//     port: process.env.DB_PORT,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME
+// });
+
+// db.connect(err => {
+//     if (err) {
+//         console.error('Erreur de connexion à la base de données :', err);
+//         throw err;
+//     }
+//     console.log('Connecté à MySQL');
+// });
+
+// module.exports = db;
--- back/server.js
+++ back/server.js
@@ -7,29 +7,25 @@ const db = require('./config/db');
 
 // initialisation de l'api
 const app = express();
-// const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extend: true}))
 
-// Mis en place du routage
-// app.get('/', (req, res) => {
-//     res.send({ message: 'Serveur opérationnel' });
-// });
-
+// Mise en place du routage
 app.get('/', (req, res) => res.send(`I'm online`))
 
 app.get('*', (req, res) => {
     res.status(501).send({ message: 'What are you doing ?!?'});
 });
 
-// Démarrer le serveur
-// app.listen(PORT, () => {
-//     console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
-// });
-
-app.listen(process.env.SERVER_PORT, () => {
-    console.log(`Serveur en cours d'exécution sur le port ${process.env.SERVER_PORT}`);
-});
+// Démarrage du serveur avec test db
+db.authenticate()
+    .then( () => console.log('Connexion à la db OK'))
+    .then( () => {
+        app.listen(process.env.SERVER_PORT, () => {
+            console.log(`Serveur en cours d'exécution sur le port ${process.env.SERVER_PORT}`);
+        });
+    })
+    .catch(err => console.log('Erreur db', err))
