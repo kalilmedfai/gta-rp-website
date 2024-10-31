a7a7fa96ee295c593a0389a452dbe1e2b668c452
kalilmedfai <kalilmedfai@hotmail.fr>
2024-11-01 00:49:10 +0100

    logout

--- front/src/components/admin/Header.js
+++ front/src/components/admin/Header.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { accountService } from '../../_services/accountService';
 
 const Header = () => {
+    let navigate = useNavigate()
+
+    const logout = () => {
+        accountService.logout()
+        navigate('/')
+    }
+
     return (
         <div className='AHeader'>
             ADMIN HEADER
+            <button onClick={logout}>Logout</button>
         </div>
     );
 };
