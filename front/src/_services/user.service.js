f75497f76ddaa376b73750651953f79a2eaf52f1
kalilmedfai <kalilmedfai@hotmail.fr>
2024-11-03 00:01:39 +0100

    Ajout utilisateur

--- front/src/_services/user.service.js
+++ front/src/_services/user.service.js
@@ -8,6 +8,10 @@ let getUser = (uid) => {
     return Axios.get('/users/'+uid)
 }
 
+let addUser = (user) => {
+    return Axios.put('/users', user)
+}
+
 let updateUser = (user) => {   // (user) n'est qu'un nom de variable
     return Axios.patch('/users/'+user.id, user)   // se fier au ficher dans back/routes/users pour le chemin dans patch / et user.id parce que dans ma variable user, il y a id dans mon objet dans le controleur / puis le dernier user et ce que je renvoie
 }
@@ -17,5 +21,5 @@ let deleteUser = (uid) => {
 }
 
 export const userService = {
-    getAllUsers, getUser, updateUser, deleteUser
+    getAllUsers, getUser, addUser, updateUser, deleteUser
 }
--- front/src/pages/Admin/User/UserAdd.js
+++ front/src/pages/Admin/User/UserAdd.js
@@ -1,9 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { userService } from '../../../_services/user.service';
 
 const UserAdd = () => {
+    // State pour être synchronisé avec html
+    const [user, setUser] = useState([])
+    let navigate = useNavigate()
+
+    // Handle modification dans le formulaire
+    const onChange = (e) => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    // Soumission du formulaire
+    const onSubmit = (e) => {
+        e.preventDefault()
+        console.log(user)
+        userService.addUser(user)
+            .then(res => {
+                navigate('../index')
+            })
+            .catch(err => console.log(err))
+    }
+
     return (
         <div className='UserAdd'>
             USER ADD
+            <form onSubmit={onSubmit}>
+                <div className='group'>
+                    <label htmlFor='pseudo'>pseudo</label>
+                    <input type='text' name='pseudo' id='pseudo' value={user.pseudo} onChange={onChange}/>
+                </div>
+
+                <div className='group'>
+                    <label htmlFor='email'>Email</label>
+                    <input type='text' name='email' id='email' value={user.email} onChange={onChange}/>
+                </div>
+
+                <div className='group'>
+                    <label htmlFor='password'>Mot de passe</label>
+                    <input type='password' name='password' id='password' value={user.password} onChange={onChange}/>
+                </div>
+
+                <div className='group'>
+                    <label htmlFor='citizenId'>ID Citizen</label>
+                    <input type='text' name='citizenId' id='citizenId' value={user.citizenId} onChange={onChange}/>
+                </div>
+                <div className='group'>
+                    <button>Ajouter</button>
+                </div>
+            </form>
         </div>
     );
 };
