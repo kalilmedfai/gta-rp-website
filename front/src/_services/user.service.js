33f9e32fe41acde5b63279e64058a64b52f1ab7a
kalilmedfai <kalilmedfai@hotmail.fr>
2024-11-02 17:42:07 +0100

    Edition utilisateur

--- front/src/_services/user.service.js
+++ front/src/_services/user.service.js
@@ -8,6 +8,10 @@ let getUser = (uid) => {
     return Axios.get('/users/'+uid)
 }
 
+let updateUser = (user) => {   // (user) n'est qu'un nom de variable
+    return Axios.patch('/users/'+user.id, user)   // se fier au ficher dans back/routes/users pour le chemin dans patch / et user.id parce que dans ma variable user, il y a id dans mon objet dans le controleur / puis le dernier user et ce que je renvoie
+}
+
 export const userService = {
-    getAllUsers, getUser
+    getAllUsers, getUser, updateUser
 }
--- front/src/pages/Admin/User/UserEdit.js
+++ front/src/pages/Admin/User/UserEdit.js
@@ -1,39 +1,52 @@
 import React, {useEffect, useRef, useState} from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { userService } from '@/_services';
 
 const UserEdit = () => {
     // State pour être synchronisé avec html
-    const [user, setUsers] = useState([])
+    const [user, setUser] = useState([])
     // flage pour éviter le double appel
     const flag = useRef(false)
+    let navigate = useNavigate()
 
     // Création de la proprieté car il ne peut pas être appelé directement
     const {uid} = useParams()
-    console.log(uid)
+    //console.log(uid)
 
+    // Handle modification dans le formulaire
     const onChange = (e) => {
-
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
     }
 
+    // Soumission du formulaire
     const onSubmit = (e) => {
-
+        e.preventDefault()
+        console.log(user)
+        userService.updateUser(user)
+            .then(res => {
+                navigate('../index')
+            })
+            .catch(err => console.log(err))
     }
 
     useEffect(() => {
         if(flag.current === false){
             // Déclenche la récupérations des utilisateurs
             userService.getUser(uid) 
-            .then(res => {
-                console.log(res.data)
-                // res.data affiche dans Components => User un objet data d'où le res.data.data
-                setUsers(res.data.data)
-            })
-            .catch(err => console.log(err))
+                .then(res => {
+                    console.log(res.data)
+                    // res.data affiche dans Components => User un objet data d'où le res.data.data
+                    setUser(res.data.data)
+                })
+                .catch(err => console.log(err))
         }
         
         // Methode de nettoyage pour éviter d'avoir 2 fois le double appel
         return () => flag.current = true
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     return (
@@ -55,7 +68,7 @@ const UserEdit = () => {
                     <input type='text' name='citizenId' id='citizenId' value={user.citizenId} onChange={onChange}/>
                 </div>
                 <div className='group'>
-                    <button>Editer</button>
+                    <button>Modifier</button>
                 </div>
             </form>
         </div>
